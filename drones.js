e92bdaea4c3a6103dfaa567bdb61ead5dc8f632d
deepjoydeb9863 <140600540+deepjoydeb9863@users.noreply.github.com>
2023-11-07 09:38:50 +0530

    drones.js

--- drones.js
+++ drones.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         productDiv.innerHTML = `
           <img src="${product.link}" alt="Product Image" class="product-image">
           <h4 class="product-info">${product.title}</h4>
-          <h4 class="product-info price">Price: $${product.price}</h4>
+          <h4 class="product-info price">Price: $Rs.{product.price}</h4>
           <h4 class="product-info">Rating: ${product.rating}</h4>
           <a href="${product['Buy Now']}" target="_blank" class="buy-button">Buy Now</a>
         `;
